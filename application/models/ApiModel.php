6ed089a79fe57b3ea4c8940ed889b117ebc6c8bc
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-03-11 01:12:47 -0300

    alterando put

--- application/models/ApiModel.php
+++ application/models/ApiModel.php
@@ -41,7 +41,8 @@ class ApiModel extends CI_Model{
         $this->uf = $data['uf'];
         $this->foto = $data['foto'];
         $this->db->where('id', $id);
-		$result = $this->db->update('aluno', $this);
+        $result = $this->db->update('aluno', $this);
+        // console.log($this->post('nome'))
        // $result = $this->db->update('aluno',$this,array('id' => $id));
          if($result)
          {
