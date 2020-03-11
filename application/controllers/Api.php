0c0052ac55d0b6362dbfb6ce54be2122e5abcd59
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-03-10 22:29:57 -0300

    subindo api restful

---
+++ application/controllers/Api.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script acess allowed');
+require(APPPATH.'/libraries/REST_Controller.php');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+class Api extends REST_Controller{
+    
+       
+    function __construct() {
+        parent::__construct();
+        $this->load->model('ApiModel');
+    }
+
+    
+    public function alunos_get($id = 0)
+	{
+        if(!empty($id)){
+            $data = $this->db->get_where("aluno", ['id' => $id])->row_array();
+        }else{
+            $data = $this->ApiModel->read();
+        }
+     
+        $this->response($data, REST_Controller::HTTP_OK);
+    }
+
+     function alunos_post(){
+        $data = array(
+            'nome' => $this->post('nome'),
+            'endereco' => $this->post('endereco'),
+            'numero' => $this->post('numero'),
+            'bairro' => $this->post('bairro'),
+            'cidade' => $this->post('cidade'),
+            'uf' => $this->post('uf'),
+            'foto' => $this->post('foto')
+        );
+        $r = $this->ApiModel->insert($data);
+        $this->response($r); 
+    }
+
+    public function alunos_put(){
+        $id = $this->uri->segment(3);
+
+        $data = array(
+            'nome' => $this->put('nome'),
+            'endereco' => $this->put('endereco'),
+            'numero' => $this->put('numero'),
+            'bairro' => $this->put('bairro'),
+            'cidade' => $this->put('cidade'),
+            'uf' => $this->put('uf'),
+            'foto' => $this->put('foto')
+        );
+         $r = $this->ApiModel->update($id,$data);
+         $this->response($r); 
+    }
+    public function alunos_delete(){
+        $id = $this->uri->segment(3);
+        $r = $this->ApiModel->delete($id);
+        $this->response($r); 
+    }
+  
+
+      
+}
+
+?>
---
+++ application/models/ApiModel.php
@@ -0,0 +1,81 @@
+<?php
+defined('BASEPATH') OR exit('No direct script acess allowed');
+
+class ApiModel extends CI_Model{
+
+    private $aluno = 'aluno';
+
+    //lista todos alunos
+    public function read(){
+        $query = $this->db->get('aluno');
+        return $query->result();
+    }
+
+   public function insert($data){
+
+        $this->nome = $data['nome'];
+        $this->endereco  = $data['endereco'];
+        $this->numero = $data['numero'];
+        $this->bairro = $data['bairro'];
+        $this->cidade = $data['cidade'];
+        $this->uf = $data['uf'];
+        $this->foto = $data['foto'];
+        if($this->db->insert('aluno',$this))
+        {    
+            return 'Data is inserted successfully';
+        }
+          else
+        {
+            return "Error has occured";
+        }
+    }
+
+
+    public function update($id,$data){
+   
+        $this->nome = $data['nome'];
+        $this->endereco  = $data['endereco'];
+        $this->numero = $data['numero'];
+        $this->bairro = $data['bairro'];
+        $this->cidade = $data['cidade'];
+        $this->uf = $data['uf'];
+        $this->foto = $data['foto'];
+        $this->db->where('id', $id);
+		$result = $this->db->update('aluno', $this);
+        //  $result = $this->db->update('aluno',$this,array('id' => $id));
+         if($result)
+         {
+             return "Data is updated successfully";
+         }
+         else
+         {
+             return "Error has occurred";
+         }
+     }
+
+     public function delete($id){
+   
+        $result = $this->db->query("delete from `aluno` where id = $id");
+        if($result)
+        {
+            return "Data is deleted successfully";
+        }
+        else
+        {
+            return "Error has occurred";
+        }
+    }
+     
+  
+    
+  
+
+
+
+}
+
+
+
+?>
+
+
