e4c3a737bd4fc9b2ec81bfe0f2be913518841ea8
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-03-11 01:20:35 -0300

    alterando put

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -44,13 +44,13 @@ class Api extends REST_Controller{
         $id = $this->uri->segment(3);
 
         $data = array(
-            'nome' => $this->put('nome'),
-            'endereco' => $this->put('endereco'),
-            'numero' => $this->put('numero'),
-            'bairro' => $this->put('bairro'),
-            'cidade' => $this->put('cidade'),
-            'uf' => $this->put('uf'),
-            'foto' => $this->put('foto')
+            'nome' => $this->post('nome'),
+            'endereco' => $this->post('endereco'),
+            'numero' => $this->post('numero'),
+            'bairro' => $this->post('bairro'),
+            'cidade' => $this->post('cidade'),
+            'uf' => $this->post('uf'),
+            'foto' => $this->post('foto')
         );
          $r = $this->ApiModel->update($id,$data);
          $this->response($r); 
